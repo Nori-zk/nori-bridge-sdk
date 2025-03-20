2cbe671543e72e27c9ee77b26ebe101e6012174d
Scott Phillips <scottjamesphillips@hotmail.com>
2025-03-20 22:37:35 +0000

    Top bridge

--- ui/components/panels/ScrollingBridge.tsx
+++ ui/components/panels/ScrollingBridge.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+import TopBridge from "@/public/assets/Top-Bridge.svg";
+
 const ScrollingBridge = () => {
-  return <div className="stroke-lightGreen fill-lightGreen w-full" >Hello</div>;
+  return (
+    <div className="stroke-lightGreen fill-lightGreen w-full">
+      <Image src={TopBridge} alt="bridge" className="stroke-lightGreen fill-lightGreen w-full" />
+    </div>
+  );
 };
 
 export default ScrollingBridge;
