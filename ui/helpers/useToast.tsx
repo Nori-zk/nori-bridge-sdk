fc3d377ff4f6d1d8020d0ee82e117f192d0e562f
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-05-14 23:00:05 +0100

    feat: retrun function from useToast hook

--- ui/helpers/useToast.tsx
+++ ui/helpers/useToast.tsx
@@ -4,24 +4,34 @@ import Toast, { ToastProps } from "@/components/ui/Toast/Toast";
 
 type ToastOptions = Omit<ToastProps, "id">;
 
-export function useToast({ button, ...rest }: ToastOptions) {
-  return sonnerToast.custom(
-    (id) => (
-      <Toast
-        id={id}
-        {...rest}
-        button={
-          button
-            ? {
-                label: button.label,
-                onClick: button.onClick || (() => {}),
-              }
-            : undefined
-        }
-      />
-    ),
-    {
-      duration: 5000,
-    }
-  );
+export function useToast(defaultOptions?: ToastOptions) {
+  return (options?: ToastOptions) => {
+    const mergedOptions = {
+      title: "Default Title", // Fallback for title
+      description: "Default Description", // Fallback for description
+      ...(options || defaultOptions || {}),
+    };
+
+    return sonnerToast.custom(
+      (id) => (
+        <Toast
+          id={id}
+          {...mergedOptions}
+          button={
+            options?.button || defaultOptions?.button
+              ? {
+                  label: (options?.button || defaultOptions?.button)!.label,
+                  onClick:
+                    (options?.button || defaultOptions?.button)!.onClick ||
+                    (() => {}),
+                }
+              : undefined
+          }
+        />
+      ),
+      {
+        duration: 5000,
+      }
+    );
+  };
 }
