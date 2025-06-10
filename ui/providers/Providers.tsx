b1ce0d20f442333f86999927d246b3d786d32ae9
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-06-10 09:46:00 +0100

    fix: fix import error

--- ui/providers/Providers.tsx
+++ ui/providers/Providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { ReactNode } from "react";
-import { MetaMaskWalletProvider } from "@/providers/MetaMaskWalletProvider/MetaMaskWalletProvider";
+import { MetaMaskWalletProvider } from "@/providers/MetaMaskWalletProvider/MetaMaskWalletProvider.tsx";
 import { WagminaProvider } from "wagmina";
 import { config } from "@/config/index.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
