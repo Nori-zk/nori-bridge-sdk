4aa0b82ba10a5f77a96eb0d7fcf039ad1e351dec
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2025-06-22 23:06:33 +0100

    feat: coingecko api

--- ui/components/bridge-control-card/ProgressSteps/LockTokens.tsx
+++ ui/components/bridge-control-card/ProgressSteps/LockTokens.tsx
@@ -3,6 +3,7 @@ import TextInput from "@/components/ui/TextInput.tsx";
 import { useMetaMaskWallet } from "@/providers/MetaMaskWalletProvider/MetaMaskWalletProvider.tsx";
 import { useProgress } from "@/providers/ProgressProvider/ProgressProvider.tsx";
 import { useForm } from "react-hook-form";
+import ConversionPrices from "@/components/bridge-control-card/ConversionPrices/ConversionPrices.tsx";
 
 type FormValues = {
   amount: string;
@@ -16,7 +17,12 @@ const LockTokens = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>();
+    watch,
+  } = useForm<FormValues>({
+    defaultValues: { amount: "0.001" },
+  });
+
+  const ethAmount = watch("amount");
 
   const onSubmit = async (data: FormValues) => {
     try {
@@ -36,39 +42,38 @@ const LockTokens = () => {
   };
 
   return (
-    <>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="mt-6 w-full text-white rounded-lg px-4 py-3 "
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mt-6 w-full text-white rounded-lg px-4 py-3"
+    >
+      <TextInput
+        id="amount-input"
+        {...register("amount", {
+          required: "Amount is required",
+          pattern: {
+            value: /^(0|[1-9]\d*)(\.\d+)?$/,
+            message: "Must be a valid number",
+          },
+          min: {
+            value: 0.004,
+            message: "Must be at least 0.004",
+          },
+          validate: (value) =>
+            parseFloat(value) >= 0.004 || "Must be at least 0.004",
+        })}
+        placeholder="Enter ETH amount"
+      />
+      <ConversionPrices ethAmount={parseFloat(ethAmount) || 0} />
+      {errors.amount && (
+        <p className="text-red-500 text-sm mt-1">{errors.amount.message}</p>
+      )}
+      <button
+        type="submit"
+        className="mt-6 w-full text-white rounded-lg px-4 py-3 border-white border-[1px]"
       >
-        <TextInput
-          id="amount-input"
-          {...register("amount", {
-            required: "Amount is required",
-            pattern: {
-              value: /^(0|[1-9]\d*)(\.\d+)?$/,
-              message: "Must be a valid number",
-            },
-            min: {
-              value: 0.004,
-              message: "Must be at least 0.004",
-            },
-
-            validate: (value) =>
-              parseFloat(value) >= 0.004 || "Must be at least 0.004",
-          })}
-        />
-        {errors.amount && (
-          <p className="text-red-500 text-sm mt-1">{errors.amount.message}</p>
-        )}
-        <button
-          type="submit"
-          className="mt-6 w-full text-white rounded-lg px-4 py-3 border-white border-[1px]"
-        >
-          Lock Tokens
-        </button>
-      </form>
-    </>
+        Lock Tokens
+      </button>
+    </form>
   );
 };
 
