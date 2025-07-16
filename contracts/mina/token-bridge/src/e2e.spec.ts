b33ee1c3011469a6215fb1e51597f33aecebdf66
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2025-07-16 20:30:54 +0100

    NB-10 draft of the state machine needed for waiting for minting opertunity

--- contracts/mina/token-bridge/src/e2e.spec.ts
+++ contracts/mina/token-bridge/src/e2e.spec.ts
@@ -1,6 +1,15 @@
 import { Field, Bytes } from 'o1js';
-import { PlonkProofAndConvertedProofBundle } from './proofTypes.js';
-import { wordToBytes } from '@nori-zk/proof-conversion/build/src/index.js';
+import {
+    BridgeEthFinalizationStatus,
+    BridgeLastStageState,
+    KeyTransitionStageEstimatedTransitionTime,
+    KeyTransitionStageMessageTypes,
+    Sp1ProofAndConvertedProofBundle,
+    TransitionNoticeMessageType,
+    WebSocketServiceTopicSubscriptionMessage,
+} from '@nori-zk/pts-types';
+import { wordToBytes } from '@nori-zk/proof-conversion';
+import { clearInterval } from 'node:timers';
 
 class InvertedPromise<T, E> {
     resolve: (output: T) => void;
@@ -16,19 +25,13 @@ class InvertedPromise<T, E> {
 
 describe('should perform an end to end pipeline', () => {
     async function connectWebsocket(
-        onData: (event: MessageEvent) => void,
+        onData: (event: MessageEvent<string>) => void,
         onClose: (event: CloseEvent) => void
     ): Promise<WebSocket> {
         return new Promise((resolve, reject) => {
             const webSocket = new WebSocket('wss://wss.nori.it.com');
             webSocket.addEventListener('open', (event) => {
                 console.log('WebSocket is opened', event);
-                webSocket.send(
-                    JSON.stringify({
-                        method: 'subscribe',
-                        topic: 'notices.transition.*',
-                    })
-                );
                 resolve(webSocket);
             });
 
@@ -45,7 +48,7 @@ describe('should perform an end to end pipeline', () => {
 
     async function proofConversionServiceRequest(
         inputBlockNumber: number
-    ): Promise<PlonkProofAndConvertedProofBundle> {
+    ): Promise<Sp1ProofAndConvertedProofBundle> {
         const fetchResponse = await fetch(
             `https://pcs.nori.it.com/converted-consensus-mpt-proofs/${inputBlockNumber}`
         );
@@ -95,19 +98,27 @@ describe('should perform an end to end pipeline', () => {
         }
         await new Promise((resolve, reject) =>
             child.on('close', (code) => {
-                if (code) return reject(new Error(`Process exited non zero code ${code}\n${error}`));
+                if (code)
+                    return reject(
+                        new Error(
+                            `Process exited non zero code ${code}\n${error}`
+                        )
+                    );
                 resolve(code);
             })
         );
         console.log(`Lock output:\n${data}`);
-        return;
+        console.log('----------------------');
+        const match = data.match(/Transaction included in block number: (\d+)/);
+        if (!match) return null;
+        return parseInt(match[1]);
     }
 
     beforeAll(() => {});
 
     test('connect_to_wss_and_await_message', async () => {
         const invertedPromise = new InvertedPromise();
-        function onData(event: MessageEvent) {
+        function onData(event: MessageEvent<string>) {
             console.log('Got first message', event.data);
             invertedPromise.resolve(event);
         }
@@ -133,35 +144,302 @@ describe('should perform an end to end pipeline', () => {
     });
 
     test('lock_token', async () => {
-        await lockTokens(new Field(10111011), 0.000001);
+        const block_number = await lockTokens(new Field(10111011), 0.000001);
+        console.log('block_number', block_number);
     });
 
-    test('e2e_pipeline_with_services', async () => {
-        // So what does this test look like?
-
-        // We need to lock tokens
-
-        // We need to inspect the status of the transition notifications stream to know where we are
-
-        // We need to maintain a state (waiting for finalization etc)
-
-        enum State {
-            WaitingForFinalization = 'WaitingForFinalization',
-            WaitingForMPTConsensusProof = 'WaitingForMPTConsensusProof',
-            WaitingForProofConversion = 'WaitingForProofConversion',
-            WaitingForMinaFinalization = 'WaitingForMinaFinalization',
+    /*test('timing_service', async () => {
+        const exitPromise = new InvertedPromise();
+        function onClose(event: CloseEvent) {
+            console.error('Connection closed', event);
+            exitPromise.reject(event);
         }
 
-        // But we need more substate than this would be nice to know the overall progress, and perhaps an estimated time
-        // for some of the stages.
+        let lastElapsedTime: {
+            [key in TransitionNoticeMessageType]?: number;
+        } = {};
 
-        // For finalization we could look at the last finalization emission from bridge head.... and know how many blocks left to go
-        // and use estimates for this time.
+        function onData(event: MessageEvent<string>) {
+            let data = JSON.parse(
+                event.data
+            ) as WebSocketServiceTopicSubscriptionMessage;
+            if (data.message_type === 'TransitionTiming') {
+                lastElapsedTime = data.extension;
+            }
+        }
 
-        // TransitionNoticeExtensionBridgeHeadFinalityTransitionDetected only has the slot
-        // so we would need more information than this..... we would need to know our current slot from the block number.....
+        setInterval(() => {
+            console.log(lastElapsedTime);
+        }, 1000);
 
-        // For proof conversion we could look at the last time (aka look at the stream for data while we await finalisation) and get an estimate of each stage
-        // Need to have a complete track of everything for this....
+        const webSocket = await connectWebsocket(onData, onClose);
+        webSocket.send(
+            JSON.stringify({
+                method: 'subscribe',
+                topic: 'timings.notices.transition',
+            })
+        );
+        await exitPromise.promise;
+        webSocket.close();
+    }, 1000000000);*/
+
+    test('e2e_pipeline_with_services', async () => {
+        const initPromise = new InvertedPromise<void, void>();
+        const exitPromise = new InvertedPromise();
+
+        let timings: KeyTransitionStageEstimatedTransitionTime;
+        let bridgeState: BridgeLastStageState;
+        let ethState: BridgeEthFinalizationStatus;
+
+        let highLevelState = 'initalising';
+        let subStage: string | null = null;
+        let depositBlockNumber: number;
+        let stageWaitTime: number | null = null;
+
+        let timeTrackInterval: NodeJS.Timeout;
+        let ethFinalityTickerHandler: NodeJS.Timeout;
+        let stageElapsedSecIncrementorTimeout: NodeJS.Timeout;
+
+        function getBlockToSlotDelta(): number | undefined {
+            if (
+                ethState == null &&
+                Number.isFinite(ethState.latest_finality_block_number) &&
+                Number.isFinite(ethState.latest_finality_slot)
+            )
+                return undefined;
+            return (
+                (ethState.latest_finality_slot as number) -
+                (ethState.latest_finality_block_number as number)
+            );
+        }
+
+        // TODO how to improve this estimate?
+        // Perhaps inspect how far we are from finality but if we exceed it by some margin assume we wait for the next finality before 
+        // the bridge head accepts this new state.... due to low vote counts being rejected.
+        function setEthFinalityEstimatorTicker(depositBlockNumber: number) {
+            const delta = getBlockToSlotDelta();
+            if (delta === undefined) return;
+
+            const depositSlot = depositBlockNumber + delta;
+            const roundedSlot = Math.ceil(depositSlot / 32) * 32;
+            const targetBlock = roundedSlot - delta;
+
+            const blocksRemaining =
+                targetBlock - (ethState.latest_finality_block_number as number);
+            let timeToWait = blocksRemaining * 12;
+            stageWaitTime = Math.max(0, timeToWait);
+
+            clearInterval(ethFinalityTickerHandler);
+            ethFinalityTickerHandler = setInterval(() => {
+                timeToWait -= 1;
+                stageWaitTime = Math.max(0, timeToWait);
+                if (stageWaitTime === 0) {
+                    // if we hit zero guess that it will occur in next finality time...
+                    timeToWait = 384;
+                }
+            }, 1000);
+        }
+
+        let previousFinalityBlockNumber: number | null = null;
+        function trackEthFinality() {
+            if (ethState.latest_finality_block_number === 'unknown') return;
+
+            const hasChanged =
+                ethState.latest_finality_block_number !==
+                previousFinalityBlockNumber;
+            if (!hasChanged) return;
+
+            if (
+                !['locking_tokens', 'waiting_for_eth_finality'].includes(
+                    highLevelState
+                )
+            )
+                return;
+
+            previousFinalityBlockNumber = ethState.latest_finality_block_number;
+
+            const diff =
+                depositBlockNumber - ethState.latest_finality_block_number;
+            console.log('diff', diff);
+            if (diff > 0) {
+                highLevelState = 'waiting_for_eth_finality';
+                setEthFinalityEstimatorTicker(depositBlockNumber);
+            } else {
+                const inputBlock = bridgeState.input_block_number as number;
+                const outputBlock = bridgeState.output_block_number as number;
+                if (
+                    inputBlock <= depositBlockNumber &&
+                    depositBlockNumber <= outputBlock
+                ) {
+                    // Deposit is in the current job window
+                    highLevelState = 'waiting_for_current_job_completion';
+                } else if (outputBlock < depositBlockNumber) {
+                    // Job has not yet reached the deposit
+                    highLevelState = 'waiting_for_previous_job_completion';
+                } else {
+                    // Deposit missed the job window
+                    highLevelState = 'missed_minting_oppertunity';
+                }
+                stageWaitTime = 0;
+                clearInterval(ethFinalityTickerHandler);
+                trackJobCompletion();
+            }
+        }
+
+        function onData(event: MessageEvent<string>) {
+            const data = JSON.parse(
+                event.data
+            ) as WebSocketServiceTopicSubscriptionMessage;
+            if (data.topic === 'state.eth') {
+                console.log('state.eth', data);
+                ethState = data.extension;
+                trackEthFinality();
+            } else if (data.topic === 'state.bridge') {
+                console.log('state.bridge', data);
+                bridgeState = data.extension;
+                clearInterval(stageElapsedSecIncrementorTimeout);
+                stageElapsedSecIncrementorTimeout = setTimeout(() => {
+                    (bridgeState.elapsed_sec as number) += 1;
+                }, 1000);
+                trackJobCompletion();
+            } else if (data.topic === 'timings.notices.transition') {
+                //console.log('timings.notices.transition', data);
+                timings = data.extension;
+            }
+            if (
+                ethState &&
+                ethState.latest_finality_block_number !== 'unknown' &&
+                bridgeState &&
+                timings
+            )
+                initPromise.resolve();
+        }
+
+        function trackJobCompletion() {
+            if (
+                ![
+                    'waiting_for_previous_job_completion',
+                    'waiting_for_current_job_completion',
+                ].includes(highLevelState)
+            )
+                return;
+            if (subStage === bridgeState.stageName) return;
+            if (subStage === 'EthProcessorTransactionFinalizationSucceeded') {
+                // but here we need to detect if our finalized eth status is beyond the last block number because otherwise we are again waiting for finalization before the job will
+                // resume FIXME
+                if (highLevelState === 'waiting_for_previous_job_completion') {
+                    highLevelState = 'waiting_for_current_job_completion';
+                } else if (
+                    highLevelState === 'waiting_for_current_job_completion'
+                ) {
+                    highLevelState = 'minting';
+                }
+                clearInterval(timeTrackInterval);
+                stageWaitTime = 0;
+            }
+            subStage = bridgeState.stageName;
+            let timeEstimate =
+                Number(
+                    timings[
+                        bridgeState.stageName as KeyTransitionStageMessageTypes
+                    ]
+                ) || 15;
+            timeEstimate -= bridgeState.elapsed_sec as number;
+
+            stageWaitTime = timeEstimate;
+            clearInterval(timeTrackInterval);
+            timeTrackInterval = setInterval(() => {
+                timeEstimate--;
+                stageWaitTime = timeEstimate;
+                if (
+                    subStage ===
+                        'EthProcessorTransactionFinalizationSucceeded' &&
+                    timeEstimate === -1
+                ) {
+                    // if we go negative then here we can assume that
+                    // we are awaiting eth finality (FIXME make this more robust)
+                    timeEstimate = 384;
+                    stageWaitTime = timeEstimate;
+                }
+            }, 1000);
+        }
+
+        function onClose(event: CloseEvent) {
+            console.error('Connection closed', event);
+            exitPromise.reject(event);
+        }
+
+        const webSocket = await connectWebsocket(onData, onClose);
+        webSocket.send(
+            JSON.stringify({
+                method: 'subscribe',
+                topic: 'state.eth',
+            })
+        );
+        webSocket.send(
+            JSON.stringify({
+                method: 'subscribe',
+                topic: 'state.bridge',
+            })
+        );
+        webSocket.send(
+            JSON.stringify({
+                method: 'subscribe',
+                topic: 'timings.notices.transition',
+            })
+        );
+
+        // Start printing status
+        let stageWaitPrinterHandler: NodeJS.Timeout;
+        function printStageWaitTime() {
+            clearInterval(stageWaitPrinterHandler);
+            stageWaitPrinterHandler = setInterval(() => {
+                if (subStage) {
+                    console.log(
+                        [
+                            `Deposit block number: ${
+                                depositBlockNumber ?? 'unknown'
+                            }`,
+                            `State: ${highLevelState}`,
+                            `Stage: ${subStage}`,
+                            `Stage wait time: ${stageWaitTime}`,
+                        ].join(' | ')
+                    );
+                } else {
+                    console.log(
+                        [
+                            `Deposit block number: ${
+                                depositBlockNumber ?? 'unknown'
+                            }`,
+                            `State: ${highLevelState}`,
+                            `Stage wait time: ${stageWaitTime}`,
+                        ].join(' | ')
+                    );
+                }
+            }, 1000);
+        }
+        printStageWaitTime();
+
+        await initPromise.promise;
+
+        highLevelState = 'locking_tokens';
+        depositBlockNumber = 4175324; // await lockTokens(new Field(10111011), 0.000001); //hard code this for now....
+        //highLevelState = 'waiting_for_eth_finality';
+        console.log('deposit_block_number', depositBlockNumber);
+        trackEthFinality();
+
+        await exitPromise.promise;
+        clearInterval(stageWaitPrinterHandler);
+        webSocket.close();
+
+        // OK so this will do for now just need to do the following
+        // Create an ecdsa credential + proof
+
+        // Fetch the converted proof + storage data. proof conversion finished for current window.
+
+        // Compute a merkle proof / witness of our inclusion of our deposit.
+
+        // Post window when we are allowed to mint but before the window is exceeded.... do the mint
+
     }, 1000000000);
 });
