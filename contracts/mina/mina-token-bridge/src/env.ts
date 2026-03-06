c7f7068ee6dc87163d4ad11e4b8d42179f8c92b5
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-03-06 14:42:28 +0000

    CHORE: bumping version and updating env.ts after a new mesa deployment.

--- contracts/mina/mina-token-bridge/src/env.ts
+++ contracts/mina/mina-token-bridge/src/env.ts
@@ -22,21 +22,21 @@ export const env: Partial<Record<NetworkName, Partial<Record<EnvName, Env>>>> =
         mina: {
             staging: {
                 NORI_MINA_TOKEN_BRIDGE_ADDRESS:
-                    'B62qkzWqyEtSHyEaWGQ7AaJiRHqDD9T7ryyQz6fpamwf3z3SPvAiayh',
+                    'B62qk5YCkfLCkjPuFQGgyQF4R8JqdHZTC1dnGD87b5sGABDenmm98GM',
                 NORI_MINA_TOKEN_BASE_ADDRESS:
-                    'B62qj8MuZ4efsWBfWeyDaKXukVod8BTvDtKsYq6zSQ6X5dFEELiYjqo',
+                    'B62qqrGGc3hx6PxUaoyb5bcGuYUxWVkkEdXmAVzaQULdZ5V3mcmhsSZ',
                 NORI_MINA_TOKEN_BASE_TOKEN_ID:
-                    '13548809602186658908121419225753857111133378200542457597649940477922233763750',
+                    '25752212657326802597915367750324599302227063476680490393442702566980675831190',
                 NORI_MINA_TOKEN_BRIDGE_TOKEN_ID:
-                    '23257910468495065247555306225437272846379428589965924744755704860109524722427',
+                    '18872171003784898968646463492176862693853643552219525039200848768175143970235',
                 MINA_ARCHIVE_RPC_URL: 'https://archive-node.devnet.nori.it.com',
                 MINA_RPC_NETWORK_URL:
                     'https://plain-1-graphql.mina-mesa-network.gcp.o1test.net/graphql',
                 MINA_RPC_NETWORK_ID: 'devnet',
                 MINA_ZKAPP_TRANSACTION_RPC_URL:
-                    'https://mina-zkapp-transaction-api.devnet.nori.it.com/api/transactions',
-                NORI_WSS_URL: 'wss://wss.nori.it.com',
-                NORI_PCS_URL: 'https://pcs.nori.it.com',
+                    'https://mina-zkapp-transaction-api.devnet.nori.it.com/api/transactions', // FIXME this is still not mesa!
+                NORI_WSS_URL: 'wss://wss.mesa.nori.it.com',
+                NORI_PCS_URL: 'https://pcs.mesa.nori.it.com',
             },
         },
     };
