a91e5a127b0599f73ccf1aaf373a83eb8e924ece
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-03-24 19:56:33 +0000

    FIX: Updating integration test data for new bridge head.

--- contracts/mina/mina-token-bridge/src/constructExampleProofs.ts
+++ contracts/mina/mina-token-bridge/src/constructExampleProofs.ts
@@ -1,11 +1,18 @@
-import { type CreateProofArgument } from '@nori-zk/o1js-zk-utils-new';
-import seriesExample1 from './test_examples/9578560/index.js';
-import seriesExample2 from './test_examples/9578592/index.js';
-import seriesExample3 from './test_examples/9578624/index.js';
-import seriesExample4 from './test_examples/9578720/index.js';
+import type { CreateProofArgument } from '@nori-zk/o1js-zk-utils-new';
+import { vkData } from './proofs/nodeVk.js';
+import { p0 } from './proofs/p0.js';
+import { sp1PlonkProof } from './proofs/sp1Proof.js';
+import seriesExample1 from './test_examples/9886304/index.js';
+import seriesExample2 from './test_examples/9886336/index.js';
+import seriesExample3 from './test_examples/9886368/index.js';
+import seriesExample4 from './test_examples/9886400/index.js';
 
-export function buildExampleProofCreateArgument(): CreateProofArgument {
-    return seriesExample1;
+export function buildExampleProofCreateArgument() {
+    const example: CreateProofArgument = {
+        sp1PlonkProof,
+        conversionOutputProof: { vkData, proofData: p0 },
+    };
+    return example;
 }
 
 export function buildExampleProofSeriesCreateArguments(): Array<CreateProofArgument> {
--- o1js-zk-utils/src/test-examples/sp1-mpt-proof/sp1ProofMessage.ts
+++ o1js-zk-utils/src/test-examples/sp1-mpt-proof/sp1ProofMessage.ts
@@ -1,5 +1,5 @@
 import type { NoriSP1ProofInput } from '@nori-zk/pts-types';
-import sp1ConsensusMPTPlonkProofRaw from './9578560-v5.0.0.json' with { type: 'json' };
+import sp1ConsensusMPTPlonkProofRaw from './9886304-v5.0.0.json' with { type: 'json' };
 const sp1ConsensusMPTPlonkProof = sp1ConsensusMPTPlonkProofRaw as WorkerOutputBridgeHeadMessage;
 
 type VerifiedContractStorageSlot = {
