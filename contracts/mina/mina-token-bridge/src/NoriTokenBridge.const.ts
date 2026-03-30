41afd313f1c704a49f664c875c8299fd14b2c9d3
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-03-30 15:08:00 +0100

    FIX: reducing windows to 32 as we have issues after the window met the maximum size: Account_action_state_precondition_unsatisfied.

--- contracts/mina/mina-token-bridge/src/NoriTokenBridge.const.ts
+++ contracts/mina/mina-token-bridge/src/NoriTokenBridge.const.ts
@@ -1,7 +1,7 @@
 import { Field } from 'o1js';
 
 /** Maximum number of deposit roots kept in the action window. */
-export const maxWindow = 40;
+export const maxWindow = 32;
 
 /** Minimum bridge burn amount */
 export const minBridgeBurnAmount = new Field(100);
