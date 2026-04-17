e3c332fba3f26d751ed2ba2f058f7babd426dbab
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-04-17 10:48:34 +0100

    CHORE: manual upgrade of integration test data

--- contracts/mina/eth-processor/src/constructExampleProofs.ts
+++ contracts/mina/eth-processor/src/constructExampleProofs.ts
@@ -2,10 +2,10 @@ import type { CreateProofArgument } from '@nori-zk/o1js-zk-utils';
 import { vkData } from './proofs/nodeVk.js';
 import { p0 } from './proofs/p0.js';
 import { sp1PlonkProof } from './proofs/sp1Proof.js';
-import seriesExample1 from './test_examples/10042720/index.js';
-import seriesExample2 from './test_examples/10042752/index.js';
-import seriesExample3 from './test_examples/10042784/index.js';
-import seriesExample4 from './test_examples/10042816/index.js';
+import seriesExample1 from './test_examples/10053120/index.js';
+import seriesExample2 from './test_examples/10053152/index.js';
+import seriesExample3 from './test_examples/10053184/index.js';
+import seriesExample4 from './test_examples/10053216/index.js';
 
 export function buildExampleProofCreateArgument() {
     const example: CreateProofArgument = {
