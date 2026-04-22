48857652b072dafa3dad8a30db260c120dbfb63b
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-04-22 11:57:43 +0100

    CHORE: upgrade to mesa 3.0.0 native and pushing new integration test files

--- contracts/mina/src/test_examples/sp1-mpt-proof/sp1ProofMessage.ts
+++ contracts/mina/src/test_examples/sp1-mpt-proof/sp1ProofMessage.ts
@@ -1,5 +1,5 @@
 import type { NoriSP1ProofInput } from '@nori-zk/pts-types';
-import sp1ConsensusMPTPlonkProofRaw from './10080800-v6.1.0.json' with { type: 'json' };
+import sp1ConsensusMPTPlonkProofRaw from './10080800.json' with { type: 'json' };
 const sp1ConsensusMPTPlonkProof = sp1ConsensusMPTPlonkProofRaw as WorkerOutputBridgeHeadMessage;
 
 type VerifiedContractStorageSlot = {
