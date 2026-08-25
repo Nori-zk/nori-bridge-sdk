8719ebf5c33945e533f800063558b2c6803bf17e
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-08-25 19:27:34 +0100

    CHORE: fixing intergration test data for new sp1 program and updating pi0

--- contracts/mina/src/constructExampleProofs.ts
+++ contracts/mina/src/constructExampleProofs.ts
@@ -2,10 +2,10 @@ import type { CreateProofArgument } from '@nori-zk/o1js-zk-utils';
 import { vkData } from './proofs/nodeVk.js';
 import { p0 } from './proofs/p0.js';
 import { sp1PlonkProof } from './proofs/sp1Proof.js';
-import seriesExample1 from './test_examples/10950400/index.js';
-import seriesExample2 from './test_examples/10950432/index.js';
-import seriesExample3 from './test_examples/10950464/index.js';
-import seriesExample4 from './test_examples/10950496/index.js';
+import seriesExample1 from './test_examples/10995104/index.js';
+import seriesExample2 from './test_examples/10995136/index.js';
+import seriesExample3 from './test_examples/10995168/index.js';
+import seriesExample4 from './test_examples/10995200/index.js';
 
 export function buildExampleProofCreateArgument() {
     const example: CreateProofArgument = {
--- contracts/mina/src/test_examples/sp1-mpt-proof/sp1ProofMessage.ts
+++ contracts/mina/src/test_examples/sp1-mpt-proof/sp1ProofMessage.ts
@@ -1,5 +1,5 @@
 import type { NoriSP1ProofInput } from '@nori-zk/pts-types';
-import sp1ConsensusMPTPlonkProofRaw from './10950400-v6.1.0.json' with { type: 'json' };
+import sp1ConsensusMPTPlonkProofRaw from './10995104-v6.1.0.json' with { type: 'json' };
 
 const sp1ConsensusMPTPlonkProof = {
     ...sp1ConsensusMPTPlonkProofRaw,
